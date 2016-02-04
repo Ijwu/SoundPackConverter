8307f2be61272d77e8d3ca512ba6e7c290cf2fd7
Hussein Farran <hmfarran1@hawkmail.hfcc.edu>
2016-02-04 18:04:15 -0500

    The start of a new era. dundundunnnnnnnn Starting WPF ConfigEditor with MVVM/

---
+++ SoundPackConverter.ConfigEditor.WPF/EditorWindowViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Windows.Documents;
+
+namespace SoundPackConverter.ConfigEditor.WPF
+{
+    public class EditorWindowViewModel
+    {
+        public List<string> SoundNames = new List<string> {"1","2"}; 
+    }
+}
